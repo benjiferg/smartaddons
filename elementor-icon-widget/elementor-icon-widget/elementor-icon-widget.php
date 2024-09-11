2c40ab917a8902e65b1fad3f24670909284baefb
benjiferg <47128986+benjiferg@users.noreply.github.com>
2024-09-11 10:21:54 -0400

    Update elementor-icon-widget.php

--- elementor-icon-widget/elementor-icon-widget/elementor-icon-widget.php
+++ elementor-icon-widget/elementor-icon-widget/elementor-icon-widget.php
@@ -28,28 +28,28 @@ add_action( 'elementor/elements/categories_registered', 'add_custom_widget_categ
 
 // Register the icon widget
 function register_icon_widget( $widgets_manager ) {
-    require_once( __DIR__ . '/widgets/icon-widget.php' );
+    require_once( __DIR__ . '/php/icon-widget.php' );
     $widgets_manager->register( new \Elementor_Icon_Widget() );
 }
 add_action( 'elementor/widgets/register', 'register_icon_widget' );
 
 // Register the shopping cart widget
 function register_shopping_cart_widget( $widgets_manager ) {
-    require_once( __DIR__ . '/widgets/shopping-cart-widget.php' );
+    require_once( __DIR__ . '/php/shopping-cart-widget.php' );
     $widgets_manager->register( new \Elementor_Shopping_Cart_Widget() );
 }
 add_action( 'elementor/widgets/register', 'register_shopping_cart_widget' );
 
 // Register the search button widget
 function register_search_button_widget( $widgets_manager ) {
-    require_once( __DIR__ . '/widgets/class-elementor-search-button-widget.php' );
+    require_once( __DIR__ . '/php/class-elementor-search-button-widget.php' );
     $widgets_manager->register( new \Elementor_Search_Button_Widget() );
 }
 add_action( 'elementor/widgets/register', 'register_search_button_widget' );
 
 // Register the account widget
 function register_account_widget( $widgets_manager ) {
-    require_once( __DIR__ . '/widgets/class-elementor-account-widget.php' );
+    require_once( __DIR__ . '/php/class-elementor-account-widget.php' );
     $widgets_manager->register( new \Elementor_Account_Widget() );
 }
 add_action( 'elementor/widgets/register', 'register_account_widget' );
